d6053d64b44a2b53c1a0b8f6784fd55522ab3bd4
mustafacavusoglu <mustafacavussoglu@gmail.com>
2023-05-19 18:58:03 +0300

    two sum added

---
+++ twoSum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hash_table:
+                return [hash_table[diff], i]
+            hash_table[n] = i
+        
