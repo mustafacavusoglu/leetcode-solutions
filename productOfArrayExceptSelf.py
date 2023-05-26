3640726595ed49ab3eeb08bfa63f385c1c0d8977
mustafacavusoglu <mustafacavussoglu@gmail.com>
2023-05-26 23:02:13 +0300

    product of array except self

---
+++ productOfArrayExceptSelf.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        prefix = 1
+
+        for i in range(len(nums)):
+            result[i] = prefix 
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            result[i] *= postfix 
+            postfix *= nums[i]
+        return result
+
+
+result = productExceptSelf([1,2,3,4])
